98ba4afe039791cbcf49172e1e85f31ef189fb63
vani33 <kothapallivanisree292@gmail.com>
2022-02-26 11:48:47 +0530

    first commit

---
+++ src/GitCommitDemoExample.java
@@ -0,0 +1,10 @@
+
+public class GitCommitDemoExample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi, I am practising git commits");
+		
+		System.out.println("Doing first commit");
+	}
+}
